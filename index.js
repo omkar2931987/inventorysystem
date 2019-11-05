dd1a9441c8f184025937124ec2842873e8d97194
omkar.ambre <omkar.ambre@sts.in>
2019-11-05 14:57:41 +0530

    committing create product expressjs code in products.js and latest db file

--- index.js
+++ index.js
@@ -1,7 +1,58 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var express = require('express');
+var mysql = require('mysql');
+var app = express();
+const port = 3001
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+
+
+var bodyParser = require('body-parser');
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.use(bodyParser.json());
+ app.use(bodyParser.urlencoded({
+     extended: true
+ }));
+
+ module.exports = app;
+
+ // connection configurations
+ var dbConn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'inventorysystem'
+});
+// connect to database
+dbConn.connect(); 
+
+
+
+// Adding New Products in inventory system  
+app.post('/createproduct', function (req, res) {
+    let productnameval = req.body.productname;
+    let categoryval = req.body.categoryname;
+    let supplierval = req.body.suppliername;
+    let cost_priceval = req.body.costpricename;
+    let priceval = req.body.pricename;
+    let unitval = req.body.unitname;
+
+    console.log(supplierval);
+
+    var createproductsql = "INSERT INTO products (name,category,supplier_name,cost_price,price,unit) VALUES ('"+productnameval+"','"+categoryval+"','"+supplierval+"','"+cost_priceval+"','"+priceval+"','"+unitval+"')";
+
+    console.log(createproductsql);
+
+    dbConn.query(createproductsql, function (err, result) {
+        if (err) throw err;
+        console.log("product created");
+    });
+});
