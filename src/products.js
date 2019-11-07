cac957eb82437a6cc43222438612ab4c2a15786a
omkar.ambre <omkar.ambre@sts.in>
2019-11-07 14:08:15 +0530

    Committing file upload changes using npm multer module

--- src/products.js
+++ src/products.js
@@ -5,10 +5,15 @@ class Products extends React.Component{
 
     constructor(props) {
         super(props);
-        this.state = {productname: '',categoryname:'', suppliername:'', costpricename:'', pricename:"",unitname:""};
+        this.state = {productname: '',categoryname:'', suppliername:'', costpricename:'', pricename:"",unitname:"",file: null};
     
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.onChange = this.onChange.bind(this);
+      }
+
+      onChange(e) {
+        this.setState({file:e.target.files[0]});
       }
 
       handleChange(event) {
@@ -18,32 +23,52 @@ class Products extends React.Component{
       handleSubmit(event) {
         event.preventDefault();
 
-        console.log(this.state);
+        //console.log(this.state);
 
-        const { productname, categoryname, suppliername, costpricename, pricename, unitname } = this.state;
-
-
+        const { productname, categoryname, suppliername, costpricename, pricename, unitname, file } = this.state;
+       
         const productdata = {
             productname,
             categoryname,
             suppliername,
             costpricename,
             pricename,
-            unitname
+            unitname            
         };
-
+        
     axios
       .post('http://localhost:3001/createproduct',productdata)
       .then(() => console.log('Product Created'))
       .catch(err => {
         console.error(err);
       });
+
+    const formData = new FormData();
+   
+      //console.log(formData);
+   
+     formData.append('myImage',this.state.file);
+
+     const config = {
+      headers: {
+          'content-type': 'multipart/form-data'
       }
+    };
+    
+    axios.post("http://localhost:3001/upload",formData,config)
+      .then((response) => {
+          alert("The file is successfully uploaded");
+      }).catch((error) => {
+    });
+    
+    } // End of Submit Function
+
+    
 
 
     render(){
         return (
-            <form onSubmit={this.handleSubmit}>
+            <form encType="multipart/form-data" onSubmit={this.handleSubmit}>
               <label>Name:</label>
                 <input type="text" name="productname" value={this.state.namevalue} onChange={this.handleChange} />
               
@@ -61,8 +86,11 @@ class Products extends React.Component{
               
               <label>Unit:</label>
                 <input type="text" name="unitname" value={this.state.unitvalue} onChange={this.handleChange} />
+
+              <input type="file" name="myImage" onChange= {this.onChange} />              
               
               <input type="submit" value="Submit" />
+              
             </form>
           );
     }
